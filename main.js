5997340b59b7bffc157c18617e0defc5dca78692
Rafiki <zangana.rano@gmail.com>
2022-02-25 14:23:05 +0100

    Hej
    Ändrade struktur på HTML så den ser ut som den ska va och remove-knappen funkar nu för elementen

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let todos = "";
 let template = document.querySelector('#todo-template');
+var todoList;
 start();
 
-function start(){
+function start() {
     template.remove();
     setSearchForm();
 }
@@ -19,8 +20,15 @@ function setSearchForm() {
     }
 }
 function addTodos() {
-    let todoList = document.querySelector('#todo-list');
+    todoList = document.querySelector('#todo-list');
     let li = template.content.firstElementChild.cloneNode(true);
     li.querySelector('.todo-value').textContent = todos;
     todoList.append(li);
+    removeTodos(li);
+}
+function removeTodos(li){
+    let todoRemove = li.querySelector('.todo-remove');
+    todoRemove.onclick = function (event) {
+        li.remove();
+    }
 }
