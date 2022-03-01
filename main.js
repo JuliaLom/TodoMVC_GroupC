5a27bb5ba16966bf0fba53c29db0f58160705141
MichaelKimelman <91721421+MichaelKimelman@users.noreply.github.com>
2022-03-01 16:17:33 +0100

    CheckBox function, Toggleall edit, RemoveChecked function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let todos = "";
 let template = document.querySelector('#todo-template');
-var todoList;
+var todoList = [];
 start();
 
 function start() {
@@ -8,6 +8,7 @@ function start() {
     template.remove();
     setSearchForm();
     ToggleAll();
+    RemoveChecked();
 }
 
 function setSearchForm() {
@@ -16,10 +17,11 @@ function setSearchForm() {
 
     form.onsubmit = function (event) {
         event.preventDefault();
-            todos = newTodos.value; //global
-            addTodos();
-            form.reset();
+        todos = newTodos.value; //global
+        addTodos();
+        form.reset();
     }
+
 }
 function addTodos() {
 
@@ -28,6 +30,7 @@ function addTodos() {
     li.querySelector('.todo-value').textContent = todos;
     todoList.append(li);
     removeTodos(li);
+    CheckBox(li);
 }
 function removeTodos(li) {
     let todoRemove = li.querySelector('.todo-remove');
@@ -37,23 +40,25 @@ function removeTodos(li) {
 }
 function ToggleAll() {
     let ToggleAllCheckbox = document.querySelector('#toggle-all');
-
     ToggleAllCheckbox.onclick = event => {
         let ul = document.querySelector('#todo-list');
         let ListofLi = ul.querySelectorAll('li');
         if (ListofLi.length > 0) {
 
             for (let li of ListofLi) {
-
+                let text = li.querySelector('.todo-value');
+                let checkbox = li.querySelector('.toggle-item');
                 if (ToggleAllCheckbox.checked == true) {
 
-                    let checkbox = li.querySelector('.toggle-item');
+                    /*  let checkbox = li.querySelector('.toggle-item'); */
                     checkbox.checked = true;
+                    text.style.color = "grey";
                 }
                 if (ToggleAllCheckbox.checked == false) {
 
-                    let checkbox = li.querySelector('.toggle-item');
+                    /* let checkbox = li.querySelector('.toggle-item'); */
                     checkbox.checked = false;
+                    text.style.color = "black";
                 }
             }
         }
@@ -61,3 +66,39 @@ function ToggleAll() {
     }
 
 }
+function CheckBox(li) {
+    let checkbox = li.querySelector('.toggle-item');
+    let text = li.querySelector('.todo-value');
+    checkbox.onclick = event => {
+
+        if (checkbox.checked == true) {
+            checkbox.checked = true;
+            text.style.color = "grey";
+        }
+        if (checkbox.checked == false) {
+            checkbox.checked = false;
+            text.style.color = "black";
+        }
+
+    }
+}
+function RemoveChecked() {
+    let button = document.querySelector('.clear-complete');
+
+    button.onclick = event => {
+        /* alert('hello'); */
+        let ul = document.querySelector('#todo-list');
+        let ListofLi = ul.querySelectorAll('li');
+        /* let checkbox = document.querySelector('.toggle-item'); */
+
+        for (let li of ListofLi) {
+            let checkbox = li.querySelector('.toggle-item');
+            if (checkbox.checked == true) {
+                li.remove();
+            }
+        }
+
+    }
+
+}
+
